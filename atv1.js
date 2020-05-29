26bb419beb0b100b403d3418e5c516960e5a5238
Eduardo <eduardo.juninho.junior@gmail.com>
2020-05-28 21:56:42 -0300

    Java atividade A,B

---
+++ atv1.js
@@ -0,0 +1,44 @@
+let receitafavorita = {
+    titulo: "Porção de Batata Frita",
+    porcoes: 1,
+    ingredientes: ["batata", "oleo", "panela","fogão","sal"]
+}
+console.log(receitafavorita.titulo);
+console.log(receitafavorita.porcoes);
+for(1=0; 1<receitafavorita.ingredientes.lenght; i++){
+    console.log(receitafavorita.ingredientes[i]);
+}
+
+
+let livros = new Array [
+    {
+    id: 1, 
+        titulo: "Harry Potter e a camara secreta",
+        autor: "J. K. Rowling",
+        livrolido: "False",
+    },
+    {
+    id: 2,
+        titulo: "As vantagens de ser insivisel",
+        autor: "Stephen Chbosky",
+        livrolido: "true",
+    },
+    {
+    id : 3, 
+        titulo: "Pequeno principe",
+        autor:  "Antoine de Saint-Exupéry",
+        livrolido: "false",
+    },
+    {
+    id : 4, 
+        titulo: "A cabana",
+        autor: "William P. Young",
+        livrolido: "false",
+    },
+    {
+    id : 5, 
+        titulo: "Turma da monica",
+        autor: "Mauricio de Sousa",
+        livrolido: "false",
+    }
+]
